e52ee423b1b9ef0f06279ef54d4c677c483d72cd
DorianLaycock <119632701+DorianLaycock@users.noreply.github.com>
2024-01-16 15:07:53 -0700

    API works in console log!

--- app/item.js
+++ app/item.js
@@ -1,9 +1,5 @@
 export default function Item({ artist, album, onDelete }) {
 
-    function onMouseOver(){
-        console.log(String(artist + " " + album));
-      }
-
     return (
         <div>
             <h3 class="cursor-pointer font-bold text-xl hover:underline">{artist}</h3>
@@ -13,4 +9,23 @@ export default function Item({ artist, album, onDelete }) {
             </p>
         </div>
     );
+
+    function onMouseOver(){
+        //console.log(String(artist + " " + album));
+
+        const apiKey = '49f75ad538e6137b8ef970367d3e8b8a';
+        const apiUrl = `http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${apiKey}&artist=${artist}&album=${album}&format=json`;
+        
+
+        fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+        // Handle the data, which contains album information including cover URLs
+        console.log(data);
+        })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+      }
+
 }
