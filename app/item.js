0637599ccb5bec87e8ad60cfa7d91b19d2244dfa
DorianLaycock <119632701+DorianLaycock@users.noreply.github.com>
2024-01-17 10:50:33 -0700

    New design choice, API working

--- app/item.js
+++ app/item.js
@@ -1,31 +1,47 @@
-export default function Item({ artist, album, onDelete }) {
+import { useState, useEffect } from "react";
 
-    return (
-        <div>
-            <h3 class="cursor-pointer font-bold text-xl hover:underline">{artist}</h3>
-            <p onMouseOver={(onMouseOver)}>{album}</p>
-            <p>
-                <button class="cursor-pointer text-red-600" onClick={() => onDelete(album)}>Remove from list</button>
-            </p>
-        </div>
-    );
+const Item = ({ artist, album, onDelete }) => {
+    const [albumInfo, setAlbumInfo] = useState(null);
 
-    function onMouseOver(){
-        //console.log(String(artist + " " + album));
+    useEffect(() => {
 
         const apiKey = '49f75ad538e6137b8ef970367d3e8b8a';
         const apiUrl = `http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${apiKey}&artist=${artist}&album=${album}&format=json`;
         
 
         fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-        // Handle the data, which contains album information including cover URLs
-        console.log(data);
-        })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-    });
-      }
+            .then(response => response.json())
+            .then(data => {
 
-}
+            // Handle the data, which contains album information including cover URLs
+            
+            const coverUrl = data.album.image[3]['#text'];
+            const releaseDate = data.album.wiki ? data.album.wiki.published : null;
+            setAlbumInfo({ artist, album, coverUrl });
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+      }, [artist, album]);
+
+
+    return (
+        <div className="flex flex-row">
+            <div>
+                {albumInfo && (
+                    <img src={albumInfo.coverUrl} alt="Album Cover" class="object-contain h-20 w-20 m-2"/>
+                )}
+            </div>
+            <div>
+                <h3 class="cursor-pointer font-bold text-xl hover:underline">{artist}</h3>
+                <p>{album}</p>
+                <p>
+                    <button class="cursor-pointer text-red-600" onClick={() => onDelete(album)}>Remove from list</button>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Item;
--- app/page.js
+++ app/page.js
@@ -26,7 +26,7 @@ export default function Page() {
   return (
     <main class="flex flex-col justify-center items-center w-screen">
       <h1 class="text-4xl">Music Catalogue</h1>
-      <p>Welcome to Music Catalogue v0.4</p>
+      <p>Welcome to Music Catalogue v0.5</p>
       <p>This is a personal project I will be using to practice my web development skills.</p>
       <p>Soon the features will be added to save your list, and display album art.</p>
       <div class="flex flex-row">
